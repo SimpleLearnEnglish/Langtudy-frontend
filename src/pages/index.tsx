b1fcb90e4d0659b71489220bcfded39a61dfa3bc
[1O16] <youiiyou1253@gmail.com>
2023-01-27 15:45:57 +0900

    fix: husky setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,7 @@ import React, { NextPage } from 'next';
 const HomePage: NextPage = () => {
   return (
     <>
-      <p>
-        아직 서비스 준비 중이에요!
-        <br /> 곧 좋은 모습으로 찾아 뵐게요 :)
-      </p>
+      <p>아직 서비스 준비 중이에요!</p>
     </>
   );
 };
