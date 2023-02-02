8c5d546c4cdd4f58c6e806810b52672d0fd54c58
alpha0212 <qkrcjfgml321@naver.com>
2023-02-02 10:30:01 +0900

    🔨 Layout: Modify FormUrl

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import Navbar from '../common/Header/Navbar';
 import * as S from './styled';
 import Menu from '../common/Header/Menu';
+import { useRouter } from 'next/router';
 
 export interface LayoutTypes {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutTypes> = ({ children }) => {
+  const Location = useRouter();
+  const ShowHeaderBoolean = Location.pathname.split('/')[1];
   return (
     <>
-      <Navbar />
-      <Menu LoginUrl="#" RegisterUrl="#" />
+      {ShowHeaderBoolean === 'auth' ? (
+        <></>
+      ) : (
+        <>
+          <Navbar />
+          <Menu LoginUrl="/auth/login" RegisterUrl="/auth/register" />
+        </>
+      )}
       {children}
     </>
   );
