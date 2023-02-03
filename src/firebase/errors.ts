a132e5734351f6752a8b2d5e1f3e4ef42796105f
alpha0212 <qkrcjfgml321@naver.com>
2023-02-03 21:34:09 +0900

    ✨ Firebase Initial Setting

---
+++ src/firebase/errors.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_ERRORS = {
+  'Firebase: Error (auth/email-already-in-use).':
+    '이미 회원가입이 되어 있는 이메일입니다.',
+  'Firebase: Error (auth/user-not-found).': '이메일이 존재하지 않습니다.',
+  'Firebase: Error (auth/wrong-password).': '비밀번호가 틀렸습니다.',
+  'Firebase: Password should be at least 6 characters (auth/weak-password).':
+    '비밀번호는 최소 6자리 이상이어야 합니다.',
+};
