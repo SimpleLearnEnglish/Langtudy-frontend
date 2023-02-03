9080be82c1cbf8db593c4367a39417411cffe73c
alpha0212 <qkrcjfgml321@naver.com>
2023-02-03 21:35:56 +0900

    ✨ Level Page

---
+++ src/pages/level/[id]/styled.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const LevelPageContainer = styled.div`
+  //shape
+  width: 100%;
+
+  //position
+  display: flex;
+  flex-direction: column;
+
+  //style
+  margin-top: 18rem;
+`;
+
+export const LevelTextContainer = styled.div`
+  width: 100%;
+
+  //text
+  font-size: 2rem;
+  font-weight: 800;
+  text-align: center;
+`;
