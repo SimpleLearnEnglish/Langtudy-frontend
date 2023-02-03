944dbbb140912068c229dcc7a0e6ac4336d40eee
alpha0212 <qkrcjfgml321@naver.com>
2023-02-03 21:29:10 +0900

    ✨ Firebase Social Register Button

---
+++ src/components/Auth/SocialButton/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
+import * as S from './styled';
+import { auth } from '@/src/firebase';
+
+export interface SocialProps {
+  SocialName: string;
+  ImgSrc: string;
+  Width: number;
+  Height: number;
+}
+
+export const SocialButton: React.FC<SocialProps> = ({
+  SocialName,
+  ImgSrc,
+  Width,
+  Height,
+}) => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  return (
+    <S.Button onClick={() => signInWithGoogle()} backgroundColor="#ffffff">
+      <S.ButtonContainer>
+        <S.SocialImg src={ImgSrc} widthSize={Width} heightSize={Height} />
+        <S.ButtonText textColor="black">{SocialName}</S.ButtonText>
+      </S.ButtonContainer>
+    </S.Button>
+  );
+};
---
+++ src/components/Auth/SocialButton/styled.ts
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button<{ backgroundColor: string }>`
+  width: 34rem;
+  display: flex;
+
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 0.6rem;
+  background-color: ${(props) => props.backgroundColor};
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  margin: auto;
+`;
+
+export const ButtonText = styled.div<{ textColor: string }>`
+  color: ${(props) => props.textColor};
+  font-size: 1.5rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  margin-left: 0.5rem;
+  text-align: center;
+`;
+
+export const SocialImg = styled.img<{
+  widthSize: number;
+  heightSize: number;
+}>`
+  width: ${(props) => props.widthSize}rem;
+  height: ${(props) => props.heightSize}rem;
+  margin: auto 0 auto -1rem;
+`;
