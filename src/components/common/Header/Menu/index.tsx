d0c09665d5a8371d82578bf6c405e3471899eadf
alpha0212 <qkrcjfgml321@naver.com>
2023-02-03 21:29:52 +0900

    ✨ Menu by page location

--- src/components/common/Header/Menu/index.tsx
+++ src/components/common/Header/Menu/index.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
 
 import * as S from './styled';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/src/firebase';
 
 type AuthLink = {
+  user: any;
+  EasyUrl: string;
+  NormalUrl: string;
+  HardUrl: string;
   LoginUrl: string;
   RegisterUrl: string;
+  PageType: string;
 };
 
-const Menu: React.FC<AuthLink> = ({ LoginUrl, RegisterUrl }) => {
+const Menu: React.FC<AuthLink> = ({
+  user,
+  EasyUrl,
+  NormalUrl,
+  HardUrl,
+  LoginUrl,
+  RegisterUrl,
+  PageType,
+}) => {
   return (
     <S.MenuContainer>
-      <S.Menus>
-        <S.MenuLink href={LoginUrl}>로그인</S.MenuLink> |
-        <S.MenuLink href={RegisterUrl}> 회원가입</S.MenuLink>
+      <S.Menus PageUrl={PageType}>
+        {PageType === 'level' ? (
+          <>
+            <S.MenuLink href={EasyUrl}>쉬움</S.MenuLink>
+            <S.MenuLink href={NormalUrl}>보통</S.MenuLink>
+            <S.MenuLink href={HardUrl}>어려움</S.MenuLink>
+          </>
+        ) : (
+          <></>
+        )}
+        {user ? (
+          <S.Logout onClick={() => signOut(auth)}>로그아웃</S.Logout>
+        ) : (
+          <>
+            <S.MenuLink href={LoginUrl}>로그인</S.MenuLink> |
+            <S.MenuLink href={RegisterUrl}> 회원가입</S.MenuLink>
+          </>
+        )}
       </S.Menus>
     </S.MenuContainer>
   );
--- src/components/common/Header/Menu/styled.ts
+++ src/components/common/Header/Menu/styled.ts
@@ -5,7 +5,7 @@ export const MenuContainer = styled.div`
   width: 100%;
   height: 4rem;
   position: fixed;
-  top: 98px;
+  top: 94px;
   right: 0;
   font-weight: bold;
   display: flex;
@@ -13,8 +13,9 @@ export const MenuContainer = styled.div`
   border-bottom: 1px solid rgba(198, 198, 198, 0.3);
 `;
 
-export const Menus = styled.div`
-  margin-right: 1rem;
+export const Menus = styled.div<{ PageUrl: string }>`
+  margin-left: ${(props) => (props.PageUrl === 'level' ? 'auto' : '0')};
+  margin-right: ${(props) => (props.PageUrl === 'level' ? 'auto' : '1rem')};
   margin-top: auto;
   margin-bottom: auto;
   font-size: 1.5rem;
@@ -28,4 +29,14 @@ export const MenuLink = styled(Link)`
   font-size: 1.5rem;
   margin-right: 0.5rem;
   margin-left: 0.5rem;
+`;
+
+export const Logout = styled.div`
+  font-weight: bold;
+  text-decoration: none;
+  color: black;
+  font-size: 1.5rem;
+  margin-right: 0.5rem;
+  margin-left: 0.5rem;
+  user-select: none;
 `;
