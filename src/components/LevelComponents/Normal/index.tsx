5efdb6d835a5edb3fe010c4caed7859471351f43
alpha0212 <qkrcjfgml321@naver.com>
2023-02-03 21:32:00 +0900

    🔨 Normal Level Component

---
+++ src/components/LevelComponents/Normal/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const Normal: React.FC = () => {
+  return <S.NormalContainer></S.NormalContainer>;
+};
---
+++ src/components/LevelComponents/Normal/styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const NormalContainer = styled.div`
+  width: 60rem;
+  height: 45rem;
+  border-radius: 1rem;
+  background-color: #ffffff;
+  margin: 3rem auto auto auto;
+`;
