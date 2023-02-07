527a8b1aab0e8b376b180c03d6c04e6638a11f87
alpha0212 <qkrcjfgml321@naver.com>
2023-02-07 22:51:47 +0900

    ✨ Make ChooseLevel Modal

---
+++ src/components/Modal/ChooseLevel/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as S from './styled';
+
+import { ModalView, authModalState } from '@/src/atoms/authModalAtom';
+import { useSetRecoilState } from 'recoil';
+import { ToggleCloseIcon } from '@/src/styles/common/styled';
+
+type ChooseLevelProps = {
+  handleClose: any;
+  moveRouter: any;
+};
+
+const ChooseLevel: React.FC<ChooseLevelProps> = ({
+  handleClose,
+  moveRouter,
+}) => {
+  return (
+    <S.LevelWarp>
+      <S.LevelContainer>
+        <S.LevelButtonContainer>
+          <S.EasyButton onClick={() => moveRouter.push('/level/easy')}>
+            <S.Text>쉬움</S.Text>
+          </S.EasyButton>
+          <S.NormalButton onClick={() => moveRouter.push('/level/normal')}>
+            <S.Text>보통</S.Text>
+          </S.NormalButton>
+          <S.HardButton onClick={() => moveRouter.push('/level/hard')}>
+            <S.Text>어려움</S.Text>
+          </S.HardButton>
+        </S.LevelButtonContainer>
+        <ToggleCloseIcon onClick={handleClose}>X</ToggleCloseIcon>
+      </S.LevelContainer>
+    </S.LevelWarp>
+  );
+};
+
+export default ChooseLevel;
---
+++ src/components/Modal/ChooseLevel/styled.ts
@@ -0,0 +1,59 @@
+import styled from '@emotion/styled';
+
+export const LevelWarp = styled.div`
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+`;
+
+export const LevelContainer = styled.div`
+  width: 60rem;
+  height: 30rem;
+  max-width: 60rem;
+  border-radius: 1rem;
+  overflow: hidden;
+  background-color: #ffffff;
+  display: flex;
+`;
+export const LevelButtonContainer = styled.div`
+  display: flex;
+  margin: auto;
+`;
+export const EasyButton = styled.button`
+  width: 12rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.5rem;
+  background: black;
+  margin: 1rem 0 1rem 1rem;
+`;
+export const NormalButton = styled.button`
+  width: 12rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.5rem;
+  background: black;
+  margin: 1rem 0 1rem 1rem;
+`;
+export const HardButton = styled.button`
+  width: 12rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.5rem;
+  background: black;
+  margin: 1rem 0 1rem 1rem;
+`;
+
+export const Text = styled.div`
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+export const LevelGif = styled.img``;
