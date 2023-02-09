cf3edcc852922a6b87fb9177677fecbd5367dcdd
alpha0212 <qkrcjfgml321@naver.com>
2023-02-09 19:30:28 +0900

    ✨ Quiz level easy

--- src/components/LevelComponents/Easy/index.tsx
+++ src/components/LevelComponents/Easy/index.tsx
@@ -2,13 +2,44 @@ import React from 'react';
 
 import * as S from './styled';
 
-// type EasyProps = {
-//   data: string;
-//   data2: string;
-//   data3: string;
-// };
+type EasyProps = {
+  dataSets: any;
+  reb: boolean;
+  answerClick: any;
+  NextNum: number;
+};
 
-export const Easy: React.FC = () => {
+export const Easy: React.FC<EasyProps> = ({
+  dataSets,
+  reb,
+  answerClick,
+  NextNum,
+}) => {
   //Will 02-08 Fin
-  return <S.EasyContainer></S.EasyContainer>;
+  return (
+    <>
+      {NextNum >= dataSets.length ? (
+        <S.EasyContainer>
+          <S.MeanContainer>문제를 모두 풀었어요!👏</S.MeanContainer>
+          <S.MeanContainer>
+            원하는 단어 추가 요청하기
+            <S.DescMenu>
+              공부하고 싶은 단어를 문제에 추가할 수 있어요!
+            </S.DescMenu>
+          </S.MeanContainer>
+        </S.EasyContainer>
+      ) : (
+        <S.EasyContainer>
+          <S.MeanContainer>{dataSets[NextNum].question}</S.MeanContainer>
+          <S.DataContainer>
+            {dataSets[NextNum].options.map((val: any, idx: number) => (
+              <S.Data onClick={() => answerClick(val.id)} key={idx}>
+                {val.option}
+              </S.Data>
+            ))}
+          </S.DataContainer>
+        </S.EasyContainer>
+      )}
+    </>
+  );
 };
--- src/components/LevelComponents/Easy/styled.ts
+++ src/components/LevelComponents/Easy/styled.ts
@@ -1,9 +1,41 @@
 import styled from '@emotion/styled';
 
 export const EasyContainer = styled.div`
-  width: 60rem;
-  height: 45rem;
+  width: 55rem;
+  height: 42.6rem;
+  border: 1px solid #0000001f;
   border-radius: 1.75rem;
   background-color: #ffffff;
   margin: 3rem auto auto auto;
+`;
+
+export const MeanContainer = styled.div`
+  margin: auto;
+  text-align: center;
+  padding: 10rem;
+  font-size: 2.3rem;
+  font-weight: 800;
+  letter-spacing: 1px;
+`;
+
+export const DataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Data = styled.div`
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  background: 'white';
+  color: 'black';
+  padding: 2.32rem;
+  text-align: center;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const DescMenu = styled.div`
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: rgba(0, 0, 0, 0.25);
 `;
