08d12350c39516f6944e2ce1d15dfc5df4f56326
alpha0212 <qkrcjfgml321@naver.com>
2023-02-10 03:04:03 +0900

    ✨ 정답 Stopwatch

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="영어, 일본어, 학교 단어 시험을 공부하고 준비하신다면 간편하게 이용해보세요!"
+        />
+        <meta name="랭터디" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/word/level/[id]/index.tsx
+++ src/pages/word/level/[id]/index.tsx
@@ -1,16 +1,11 @@
 import Level from '@/src/components/Template/Level';
 import { Easy, Normal, Hard } from 'src/components';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import {
-  useInfiniteQuery,
-  useQuery,
-  QueryClient,
-  dehydrate,
-} from 'react-query';
+import React, { use, useEffect, useState } from 'react';
 
 import * as S from './styled';
 import { getEng } from '@/src/api';
+import { SnackBar } from '@/src/components/common/Toastify';
 
 const LevelPage: React.FC = () => {
   //데이터 패칭해서 컴포넌트로 api 정보를 보냄
@@ -26,23 +21,49 @@ const LevelPage: React.FC = () => {
   ]);
 
   useEffect(() => {
-    getEng().then((v) => {
+    getEng('db').then((v) => {
       setEasy(v.easy);
     });
   }, [level]);
   const [clickedObject, setClickedObject] = useState(false);
-
+  const [toToastify, setToToastify] = useState(false);
   const [QuizN, setQuizN] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
   const clickAnswer = (event: any) => {
     if (event === easy[QuizN].answer) {
       setClickedObject(true);
+      setToToastify(true);
+      setIsRunning(!isRunning);
     } else {
       setClickedObject(false);
+      setIsRunning(true);
     }
   };
+  useEffect(() => {
+    setSeconds(0);
+  }, [QuizN]);
+  //clock
+  useEffect(() => {
+    let intervalId: any;
+    if (easy.length !== QuizN) {
+      if (isRunning) {
+        intervalId = setInterval(() => {
+          setSeconds((seconds) => seconds + 1);
+        }, 1000);
+      } else if (!isRunning && seconds !== 0) {
+        clearInterval(intervalId);
+      }
+    } else {
+      console.log('watch stop');
+    }
 
+    return () => clearInterval(intervalId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isRunning, seconds]);
   return (
     <S.LevelPageContainer>
+      <SnackBar seconds={seconds} answer={clickedObject} />
       <S.LevelTextContainer>
         난이도: {level === 'easy' ? <>쉬움</> : <></>}
         {level === 'normal' ? <>보통</> : <></>}
