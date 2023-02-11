707d93ce905f1f4af591c2404cbc2f46dc475a5e
alpha0212 <qkrcjfgml321@naver.com>
2023-02-11 21:57:14 +0900

    ✨ Alert Toastify

--- src/components/common/Toastify/index.tsx
+++ src/components/common/Toastify/index.tsx
@@ -5,31 +5,24 @@ import styled from '@emotion/styled';
 
 export const Toastify = styled(ToastContainer)`
   @media screen and (min-width: 40rem) {
-    width: 27rem;
+    width: 20rem;
   }
 `;
 
 type ToastifyType = {
   answer: boolean;
-  seconds: number;
 };
-/**
- * 몇초만에 풀었는지 알려줌
- * 처음에 false를 받기 때문에 false일 때는 아무것도 안띄우고
- * true가 되었을 때 초를 리턴함.
- *
- *
- */
-export const SnackBar: React.FC<ToastifyType> = ({ answer, seconds }) => {
+export const SnackBar: React.FC<ToastifyType> = ({ answer }) => {
   useEffect(() => {
-    answer === true ? (
-      toast.success(`${seconds}초 걸렸어요!`, {
-        autoClose: 2000,
-        position: toast.POSITION.BOTTOM_CENTER,
-      })
-    ) : (
-      <></>
-    );
+    answer
+      ? toast.success(`정답입니다!`, {
+          autoClose: 2000,
+          position: toast.POSITION.BOTTOM_CENTER,
+        })
+      : toast.error(`정답이 아닙니다!`, {
+          autoClose: 2000,
+          position: toast.POSITION.BOTTOM_CENTER,
+        });
   }, [answer]);
 
   return <Toastify />;
