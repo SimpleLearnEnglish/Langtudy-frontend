279abb6bb601395c76d855796f92348e11c45bd2
alpha0212 <qkrcjfgml321@naver.com>
2023-03-12 23:07:53 +0900

    ✨ Imgsrc Props

--- src/components/Modal/InstallAdvise/index.tsx
+++ src/components/Modal/InstallAdvise/index.tsx
@@ -3,17 +3,42 @@ import * as S from './styled';
 
 interface CloseFunction {
   handleClose(): void;
+  IosIcon: string;
+  AndroidIcon: string;
+  PCIcon: string;
 }
 
-export const InstallAdvise: React.FC<CloseFunction> = ({ handleClose }) => {
+export const InstallAdvise: React.FC<CloseFunction> = ({
+  handleClose,
+  IosIcon,
+  AndroidIcon,
+  PCIcon,
+}) => {
   return (
-    <S.ModalWarp>
+    <S.ModalWarp onClick={handleClose}>
       <S.ModalContainer>
-        <S.ModalButtonContainer>
-          <S.ModalButton onClick={handleClose}>
-            <S.ButtonText>닫기</S.ButtonText>
-          </S.ModalButton>
-        </S.ModalButtonContainer>
+        <S.CloseIconContainer>
+          <S.CloseIcon onClick={handleClose}>
+            <S.CloseIconRightDown />
+            <S.CloseIconLeftDown />
+          </S.CloseIcon>
+        </S.CloseIconContainer>
+        <S.IconBoxContainer>
+          <S.IconsBox>
+            <S.IosInstallWay src={IosIcon} />
+          </S.IconsBox>
+          <S.IconsBox>
+            <S.InstallWay src={AndroidIcon} />
+          </S.IconsBox>
+          <S.IconsBox>
+            <S.InstallWay src={PCIcon} />
+          </S.IconsBox>
+        </S.IconBoxContainer>
+        <S.AdviseInstallText>
+          위는 앱 설치방법이에요.
+          <br />
+          원활한 앱 사용을 위해 설치해주세요!
+        </S.AdviseInstallText>
       </S.ModalContainer>
     </S.ModalWarp>
   );
